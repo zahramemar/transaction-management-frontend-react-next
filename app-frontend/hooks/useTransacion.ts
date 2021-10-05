fb38fb05cda0a2cf671eb5db1d82dae92526a5d2
Zahra Memar <zahra.memar72@gmail.com>
2021-10-05 19:27:11 +0200

    Add date-fns and sort transactions

--- app-frontend/hooks/useTransacion.ts
+++ app-frontend/hooks/useTransacion.ts
@@ -1,11 +1,22 @@
 import useSWR from "swr";
 import { Transaction } from "../entities/transaction.entity";
 import { fetcher } from "../utils";
+import { compareAsc } from "date-fns/fp";
+
+function compareTransaction(a: Transaction, b: Transaction) {
+  return compareAsc(new Date(a.created_at), new Date(b.created_at));
+}
 
 export function useTransactions() {
   const { data, error } = useSWR<Transaction[]>("/api/transactions", fetcher, {
     refreshInterval: 2000,
   });
 
-  return { transactions: data, isLoading: !error && !data, error };
+  const transactions = data?.sort(compareTransaction);
+
+  return {
+    transactions,
+    isLoading: !error && !data,
+    error,
+  };
 }
