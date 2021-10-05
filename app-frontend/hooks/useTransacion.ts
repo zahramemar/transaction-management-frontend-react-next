d5b67049c753b6d364fa4e855c2fb7fbf37f9d03
Zahra Memar <zahra.memar72@gmail.com>
2021-10-05 19:06:06 +0200

    Add refreshInterval to the option for fetching transaction

--- app-frontend/hooks/useTransacion.ts
+++ app-frontend/hooks/useTransacion.ts
@@ -3,7 +3,9 @@ import { Transaction } from "../entities/transaction.entity";
 import { fetcher } from "../utils";
 
 export function useTransactions() {
-  const { data, error } = useSWR<Transaction[]>("/api/transactions", fetcher);
+  const { data, error } = useSWR<Transaction[]>("/api/transactions", fetcher, {
+    refreshInterval: 2000,
+  });
 
   return { transactions: data, isLoading: !error && !data, error };
 }
